718bcbf90c9cd198553f6d8be7effbc1713bde3f
looptood43243 <103706733+looptood43243@users.noreply.github.com>
2022-06-11 17:11:39 -0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
     async fetch(request, env) {
       let url = new URL(request.url);
       if (url.pathname.startsWith('/')) {
-        url.hostname="newlifeone.herokuapp.com";
+        url.hostname="new-twolife.herokuapp.com";
         let new_request=new Request(url,request);
         return fetch(new_request);
       }
